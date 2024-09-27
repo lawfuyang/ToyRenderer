fe48d3eac803c19be162b3dd91614770cce3e99c
lawfuyang <53224523+lawfuyang@users.noreply.github.com>
2024-09-27 12:01:17 +0800

    Update DXC to 1.8, revise help.txt and compile script
    Updated help.txt for DXC 1.8 with new version info, usage instructions, and options. Notable additions include new diagnostic, inlining, and SPIR-V options. Updated shader model versions to 6_8.

--- compileallshaders.bat
+++ compileallshaders.bat
@@ -10,9 +10,8 @@ cd /d "%~dp0"
 @SET OUT="%cd%/bin/shaders/"
 @SET INCLUDE_DIRS=--include="%cd%/" --include="%cd%/source/shaders/" --include="%cd%/extern/amd/FidelityFX/include/FidelityFX/gpu/"
 @SET RELAXED_INCLUDES=--relaxedInclude=MathUtilities.h --relaxedInclude=vaShared.hlsl --relaxedInclude=XeGTAO.h
-@SET COMPILER_OPTIONS="-HV 2021"
-rem TODO: -Wconversion
-rem TODO: -Wdouble-promotion -Whlsl-legacy-literal
+rem @SET COMPILER_OPTIONS="-Wconversion -Wdouble-promotion -Whlsl-legacy-literal"
+rem @SET COMPILER_OPTIONS="-HV 202x"
 
 %SHADER_MAKE_EXE% --platform="DXIL" --config=%CONFIG_FILE% --out=%OUT% --binaryBlob --compiler=%DXC_PATH% --shaderModel="6_5" --WX --embedPDB --matrixRowMajor %INCLUDE_DIRS% --outputExt=".bin" --continue --colorize %RELAXED_INCLUDES% --flatten --compilerOptions=%COMPILER_OPTIONS% -D FFX_GPU -D FFX_HLSL
 
