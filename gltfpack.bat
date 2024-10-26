ad232defc0f1d4d3ff76116bf612e3e8d8335760
Roger <53224523+lawfuyang@users.noreply.github.com>
2024-10-26 23:42:55 +0800

    Update gltfpack.bat

--- gltfpack.bat
+++ gltfpack.bat
@@ -1,6 +1,6 @@
-@SET INPUT="Sponza.gltf"
-@SET OUTPUT="SponzaOptimized.glb"
+@SET INPUT="Sponza"
+@SET INPUT_EXT="gltf"
 
-gltfpack.exe -i %INPUT% -o %OUTPUT% -tu -noq -v
+gltfpack.exe -i %INPUT%.%INPUT_EXT% -o %INPUT%_Optimized.glb -tu -mi -noq -v
 
 pause
