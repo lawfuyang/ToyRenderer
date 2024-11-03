f5489c20d63a32da9dd5e2d8382d77005f82119a
Roger <53224523+lawfuyang@users.noreply.github.com>
2024-11-03 22:21:53 +0800

    Update gltfpack.bat

--- gltfpack.bat
+++ gltfpack.bat
@@ -8,17 +8,11 @@ rem Loop through .gltf and .glb files in the folder
 for %%F in (*.gltf *.glb) do (
     set "filename=%%~nF"
     set "extension=%%~xF"
-    goto :found
-)
-
-:found
-if defined filename (
-    echo Filename: !filename!
+	
+	echo Filename: !filename!
     echo Extension: !extension!
 	
 	gltfpack.exe -i !filename!!extension! -o !filename!_Optimized.glb -tu -cc -v
-) else (
-    echo No .gltf or .glb files found.
 )
 
 endlocal
