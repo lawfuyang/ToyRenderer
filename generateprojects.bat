305119773dc7b646bebc8cb48377c6d4dd56c366
Roger <53224523+lawfuyang@users.noreply.github.com>
2024-11-10 15:02:28 +0800

    In generateprojects.bat, standardized comments, added checks for directory existence, introduced :DownloadAndExtractPackage function, added AMD FidelityFX SDK handling
    Updated .gitignore to include /tmp and /extern/amd/FidelityFX

--- generateprojects.bat
+++ generateprojects.bat
@@ -1,10 +1,10 @@
 @echo off
 setlocal enabledelayedexpansion
 
-rem Change to the directory containing the batch file
+:: Change to the directory containing the batch file
 cd /d "%~dp0"
 
-rem Check if CMake is installed
+:: Check if CMake is installed
 where cmake >nul 2>&1
 
 if %errorlevel% neq 0 (
@@ -14,76 +14,51 @@ if %errorlevel% neq 0 (
 
 set TMP_FOLDER=%cd%\tmp
 
-rem check & download DXC
-set DXC_EXEC=%cd%\extern\dxc\dxc.exe
+:: DXC
 set DXC_DEST_FOLDER=%cd%\extern\dxc
-if exist "%DXC_EXEC%" (
-
-    echo DXC files found in %DXC_DEST_FOLDER%
-
-    set "DESIRED_DXC_VERSION_STRING=dxcompiler.dll: 1.8 - 1.8.2407.7 (416fab6b5); dxil.dll: 1.8(101.8.2407.12)"
-    
-    :: Run dxc.exe and capture the version output
-    for /f "delims=" %%i in ('"%DXC_EXEC%" --version') do set "currentVersionString=%%i"
-    
-    if "!currentVersionString!"=="!DESIRED_DXC_VERSION_STRING!" (
-        echo Desired DXC version found. Skipping Download.
-        echo:
-        goto :AfterDownloadDXC
-    ) else (
-        echo Desired DXC version mismatch. Proceeding to download DXC and overrite existing files.
-    )
+if not exist "%DXC_DEST_FOLDER%" (
+	call :DownloadAndExtractPackage https://github.com/microsoft/DirectXShaderCompiler/releases/download/v1.8.2407/dxc_2024_07_31_clang_cl.zip dxc	
+	xcopy "%TMP_FOLDER%\dxc\bin\x64\*" "%DXC_DEST_FOLDER%\" /E /I /Y
 )
 
-call :DownloadPackage https://github.com/microsoft/DirectXShaderCompiler/releases/download/v1.8.2407/dxc_2024_07_31_clang_cl.zip dxc
-
-echo Copying DXC files to '%DXC_DEST_FOLDER%'
-mkdir "%DXC_DEST_FOLDER%" 2>nul
-xcopy "%TMP_FOLDER%\dxc\bin\x64\*" "%DXC_DEST_FOLDER%\" /E /I /Y
-
-:AfterDownloadDXC
-
-rem check & download Nvidia Aftermath
-
+:: Nvidia Aftermath
 set "AFTERMATH_DEST_FOLDER=%cd%\extern\nvidia\aftermath"
-if exist "%AFTERMATH_DEST_FOLDER%\GFSDK_Aftermath.h" (
-    echo Nvidia Aftermath files found in %AFTERMATH_DEST_FOLDER%. Skipping Download.
-	echo:
-	goto :AfterDownloadAftermath
+if not exist %AFTERMATH_DEST_FOLDER% (
+	call :DownloadAndExtractPackage https://developer.nvidia.com/downloads/assets/tools/secure/nsight-aftermath-sdk/2024_2_0/windows/NVIDIA_Nsight_Aftermath_SDK_2024.2.0.24200.zip aftermath
+
+	xcopy "%TMP_FOLDER%\aftermath\include\*" "%AFTERMATH_DEST_FOLDER%\" /E /I /Y
+	xcopy "%TMP_FOLDER%\aftermath\lib\x64\*" "%AFTERMATH_DEST_FOLDER%\" /E /I /Y
+	xcopy "%AFTERMATH_DEST_FOLDER%\GFSDK_Aftermath_Lib.x64.dll" "%cd%\bin\" /E /I /Y
 )
 
-call :DownloadPackage https://developer.nvidia.com/downloads/assets/tools/secure/nsight-aftermath-sdk/2024_2_0/windows/NVIDIA_Nsight_Aftermath_SDK_2024.2.0.24200.zip  aftermath
-
-echo Copying Nvidia Aftermath files to '%AFTERMATH_DEST_FOLDER%'
-mkdir "%AFTERMATH_DEST_FOLDER%" 2>nul
-xcopy "%TMP_FOLDER%\aftermath\include\*" "%AFTERMATH_DEST_FOLDER%\" /E /I /Y
-xcopy "%TMP_FOLDER%\aftermath\lib\x64\*" "%AFTERMATH_DEST_FOLDER%\" /E /I /Y
-xcopy "%AFTERMATH_DEST_FOLDER%\GFSDK_Aftermath_Lib.x64.dll" "%cd%\bin\" /E /I /Y
-
-:AfterDownloadAftermath
+:: AMD FidelityFX SDK
+set "FFX_DEST_FOLDER=%cd%\extern\amd\FidelityFX"
+if not exist %FFX_DEST_FOLDER% (
+	call :DownloadAndExtractPackage https://github.com/GPUOpen-LibrariesAndSDKs/FidelityFX-SDK/archive/refs/tags/v1.1.2.zip ffx
+	
+	xcopy "%TMP_FOLDER%\ffx\FidelityFX-SDK-1.1.2\sdk\include\FidelityFX\gpu\*" "%FFX_DEST_FOLDER%\include\FidelityFX\gpu\" /E /I /Y
+	xcopy "%TMP_FOLDER%\ffx\FidelityFX-SDK-1.1.2\sdk\src\backends\dx12\shaders\*" "%FFX_DEST_FOLDER%\src\backends\dx12\shaders\" /E /I /Y
+)
 
 goto :AfterDownloadPackages
 
-:DownloadPackage
+:DownloadAndExtractPackage
 set URL="%~1"
 set PACKAGE_NAME=%~2
+set "PACKAGE_ARCHIVE=%TMP_FOLDER%\%PACKAGE_NAME%.zip"
 
-echo Downloading '%PACKAGE_NAME%' to '%TMP_FOLDER%\%PACKAGE_NAME%'
-mkdir "%TMP_FOLDER%\%PACKAGE_NAME%"
-powershell -Command "Invoke-WebRequest -Uri '%URL%' -OutFile '%TMP_FOLDER%\%PACKAGE_NAME%\tmp.zip'"
+if not exist %PACKAGE_ARCHIVE% (
+	mkdir "%TMP_FOLDER%\%PACKAGE_NAME%"
+	powershell -Command "Invoke-WebRequest -Uri '%URL%' -OutFile '%PACKAGE_ARCHIVE%'"
+)
 
-echo Extracting '%PACKAGE_NAME%'
-powershell -Command "Expand-Archive -Path '%TMP_FOLDER%\%PACKAGE_NAME%\tmp.zip' -DestinationPath '%TMP_FOLDER%\%PACKAGE_NAME%'"
+powershell -Command "Expand-Archive -Path '%PACKAGE_ARCHIVE%' -DestinationPath '%TMP_FOLDER%\%PACKAGE_NAME%'"
+
 goto :eof
 
 :AfterDownloadPackages
 
-if exist %TMP_FOLDER% (
-	echo Deleting tmp folder
-	rd /S /Q %TMP_FOLDER%"
-)
-
-rem generate projects
+:: generate projects
 call :CreateProject "%cd%" "%cd%\projects\ToyRenderer"
 call :CreateProject "%cd%\extern\ShaderMake" "%cd%\projects\ShaderMake"
 goto :AfterGenerateProjects
@@ -91,7 +66,6 @@ goto :AfterGenerateProjects
 :CreateProject
 set SRC_PATH="%~1"
 set BUILD_PATH="%~2"
-echo Generating %SRC_PATH%...
 cmake -S %SRC_PATH% -B %BUILD_PATH%
 echo:
 echo:
@@ -99,7 +73,7 @@ goto :eof
 
 :AfterGenerateProjects
 
-rem create shortcuts to VS solutions in root folder
+:: create shortcuts to VS solutions in root folder
 call :CreateSlnShortcut "%cd%\projects\ToyRenderer\ToyRenderer.sln" "%cd%\ToyRenderer.sln.lnk"
 call :CreateSlnShortcut "%cd%\projects\ShaderMake\ShaderMake.sln" "%cd%\ShaderMake.sln.lnk"
 goto :AfterCreateSlnShortcuts
@@ -107,7 +81,6 @@ goto :AfterCreateSlnShortcuts
 :CreateSlnShortcut
 set TargetPath="%~1"
 set ShortcutPath="%~2"
-echo Creating shortcut for %TargetPath%
 echo Set objShell = WScript.CreateObject("WScript.Shell") >> CreateShortcut.vbs
 echo Set objShortcut = objShell.CreateShortcut(%ShortcutPath%) >> CreateShortcut.vbs
 echo objShortcut.TargetPath = %TargetPath% >> CreateShortcut.vbs
