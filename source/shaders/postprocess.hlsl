9f56c7c02ddda7a55bc152c48111780f20efcfc8
Roger <53224523+lawfuyang@users.noreply.github.com>
2025-02-08 09:50:37 +0800

    use new fancy "Neutral PBR Tonemapper" from Khronos
    -can't tell any diff from ACES tho...

--- source/shaders/postprocess.hlsl
+++ source/shaders/postprocess.hlsl
@@ -19,6 +19,29 @@ float3 ACES_Fast(float3 x)
     return (x * (a * x + b)) / (x * (c * x + d) + e);
 }
 
+// Input color is non-negative and resides in the Linear Rec. 709 color space.
+// Output color is also Linear Rec. 709, but in the [0, 1] range.
+float3 PBRNeutralToneMapping(float3 color)
+{
+    const float startCompression = 0.8 - 0.04;
+    const float desaturation = 0.15;
+
+    float x = min(color.r, min(color.g, color.b));
+    float offset = x < 0.08 ? x - 6.25 * x * x : 0.04;
+    color -= offset;
+
+    float peak = max(color.r, max(color.g, color.b));
+    if (peak < startCompression)
+        return color;
+
+    const float d = 1. - startCompression;
+    float newPeak = 1. - d * d / (peak + d - startCompression);
+    color *= newPeak / peak;
+
+    float g = 1. - 1. / (desaturation * (peak - newPeak) + 1.);
+    return mix(color, newPeak * float3(1, 1, 1), g);
+}
+
 void PS_PostProcess(
     in float4 inPosition : SV_POSITION,
     in float2 inUV : TEXCOORD0,
@@ -38,7 +61,8 @@ void PS_PostProcess(
     float fLumScale = g_PostProcessParameters.m_MiddleGray / sceneLuminance;
     rgb *= fLumScale;
     
-    rgb = ACES_Fast(rgb);
+    //rgb = ACES_Fast(rgb);
+    rgb = PBRNeutralToneMapping(rgb);
     
     rgb = LinearToSRGB(rgb);
 
