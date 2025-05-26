116b7f993fcc55d0bcb2bc22691ff488b462f08c
lawfuyang <53224523+lawfuyang@users.noreply.github.com>
2025-05-26 13:52:13 +0800

    dont generate sln shortcut in generateprojects.bat files
    add default template scene-less launch.json

--- generateprojects.bat
+++ generateprojects.bat
@@ -15,22 +15,5 @@ if %errorlevel% neq 0 (
 :: cmake call
 cmake -S "%cd%" -B "%cd%\build"
 
-:: create shortcuts to VS sln in root folder
-call :CreateSlnShortcut "%cd%\build\ToyRenderer.sln" "%cd%\ToyRenderer.sln.lnk"
-goto :AfterCreateSlnShortcuts
-
-:CreateSlnShortcut
-set TargetPath="%~1"
-set ShortcutPath="%~2"
-echo Set objShell = WScript.CreateObject("WScript.Shell") >> CreateShortcut.vbs
-echo Set objShortcut = objShell.CreateShortcut(%ShortcutPath%) >> CreateShortcut.vbs
-echo objShortcut.TargetPath = %TargetPath% >> CreateShortcut.vbs
-echo objShortcut.Save>> CreateShortcut.vbs
-cscript //nologo CreateShortcut.vbs
-del CreateShortcut.vbs
-goto :eof
-
-:AfterCreateSlnShortcuts
-
 echo:
 pause
