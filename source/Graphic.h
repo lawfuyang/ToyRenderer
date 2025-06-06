80bda9243274cf0789c8ee58023d67b819bd88e5
lawfuyang <53224523+lawfuyang@users.noreply.github.com>
2025-06-06 15:13:11 +0800

    delete GraphicRHI.h - just move the class to Graphic

--- source/Graphic.h
+++ source/Graphic.h
@@ -8,7 +8,6 @@
 #include "CriticalSection.h"
 #include "Math.h"
 #include "Visual.h"
-#include "GraphicRHI.h"
 #include "SmallVector.h"
 
 class CommonResources;
@@ -16,6 +15,23 @@ class DescriptorTableManager;
 class RenderGraph;
 class Scene;
 struct MaterialData;
+
+class GraphicRHI
+{
+public:
+    virtual ~GraphicRHI() = default;
+
+    static GraphicRHI* Create();
+    
+    virtual nvrhi::DeviceHandle CreateDevice() = 0;
+    virtual void InitSwapChainTextureHandles() = 0;
+    virtual uint32_t GetCurrentBackBufferIndex() = 0;
+    virtual void SwapChainPresent() = 0;
+    virtual void* GetNativeCommandList(nvrhi::CommandListHandle commandList) = 0;
+
+    virtual void SetRHIObjectDebugName(nvrhi::CommandListHandle commandList, std::string_view debugName) = 0;
+    virtual void SetRHIObjectDebugName(nvrhi::ResourceHandle resource, std::string_view debugName) = 0;
+};
 
 class Graphic
 {
--- source/GraphicRHI.cpp
+++ source/GraphicRHI.cpp
@@ -1,5 +1,3 @@
-#include "GraphicRHI.h"
-
 #include <d3d12.h>
 #include <dxgi1_6.h>
 
