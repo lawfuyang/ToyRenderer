f02dfabe4ef596b5c74b276eb3dd63841ed55cd2
lawfuyang <53224523+lawfuyang@users.noreply.github.com>
2025-09-17 17:57:36 +0800

    changed to 'NVSDK_NGX_D3D12_Init_with_ProjectID' from 'NVSDK_NGX_D3D12_Init' - whats the difference? dont know, dont care. both seem to work, but the former is the more "legit" function?
    get the "capability params" and check for driver & supersampling feature support

--- source/TAARenderer.cpp
+++ source/TAARenderer.cpp
@@ -11,13 +11,16 @@
         const NVSDK_NGX_Result result = x; \
         if (NVSDK_NGX_FAILED(result)) \
         { \
-            LOG_DEBUG("NGX call failed: %s", EnumUtils::ToString(result)); \
+            LOG_DEBUG("NGX call failed: %s", StringUtils::WideToUtf8(GetNGXResultAsString(result))); \
+            check(false); \
         } \
     }
 
 class TAARenderer : public IRenderer
 {
     bool m_bShutdownDone = false;
+    NVSDK_NGX_Parameter* m_Parameters = nullptr;
+
 public:
     TAARenderer() : IRenderer("TAA Renderer"){}
 
@@ -27,6 +30,10 @@ public:
         {
             return;
         }
+
+        check(m_Parameters);
+        NGX_CALL(NVSDK_NGX_D3D12_DestroyParameters(m_Parameters));
+        m_Parameters = nullptr;
         
         ID3D12Device* natived3d12Device = g_Graphic.m_NVRHIDevice->getNativeObject(nvrhi::ObjectTypes::D3D12_Device);
         NGX_CALL(NVSDK_NGX_D3D12_Shutdown1(natived3d12Device));
@@ -37,7 +44,34 @@ public:
     void Initialize() override
     {
         ID3D12Device* natived3d12Device = g_Graphic.m_NVRHIDevice->getNativeObject(nvrhi::ObjectTypes::D3D12_Device);
-        NGX_CALL(NVSDK_NGX_D3D12_Init(CompileTimeHashString64("ToyRenderer"), StringUtils::Utf8ToWide(GetExecutableDirectory()), natived3d12Device));
+
+        const char* projectID = "8f1e6c1e-83c7-44e1-9d35-5a55e26a7f74"; // MD5 hash of "ToyRenderer"
+        const char* engineVersion = "1.0.0";
+        NGX_CALL(NVSDK_NGX_D3D12_Init_with_ProjectID(projectID, NVSDK_NGX_ENGINE_TYPE_CUSTOM, engineVersion, StringUtils::Utf8ToWide(GetExecutableDirectory()), natived3d12Device));
+
+        NGX_CALL(NVSDK_NGX_D3D12_GetCapabilityParameters(&m_Parameters));
+
+        int bNeedsUpdatedDriver = 0;
+        NGX_CALL(m_Parameters->Get(NVSDK_NGX_Parameter_SuperSampling_NeedsUpdatedDriver, &bNeedsUpdatedDriver));
+
+        if (bNeedsUpdatedDriver)
+        {
+            unsigned int minDriverVersionMajor = 0;
+            unsigned int minDriverVersionMinor = 0;
+            NGX_CALL(m_Parameters->Get(NVSDK_NGX_Parameter_SuperSampling_MinDriverVersionMajor, &minDriverVersionMajor));
+            NGX_CALL(m_Parameters->Get(NVSDK_NGX_Parameter_SuperSampling_MinDriverVersionMinor, &minDriverVersionMinor));
+
+            LOG_DEBUG("NVIDIA driver update required. Minimum driver version: %u.%u", minDriverVersionMajor, minDriverVersionMinor);
+            check(false);
+        }
+
+        int bDLSS_Supported = 0;
+        NGX_CALL(m_Parameters->Get(NVSDK_NGX_Parameter_SuperSampling_Available, &bDLSS_Supported));
+        check(bDLSS_Supported);
+
+        int bDLSS_FeatureInitResult = 0;
+        NGX_CALL(m_Parameters->Get(NVSDK_NGX_Parameter_SuperSampling_FeatureInitResult, &bDLSS_FeatureInitResult));
+        check(bDLSS_FeatureInitResult);
     }
 
     void UpdateImgui() override
